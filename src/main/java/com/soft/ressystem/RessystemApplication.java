aa1e30cd03b8e7db023bc62ecf27f7d4d6568b2b
otsojm <62076266+otsojm@users.noreply.github.com>
2021-10-28 18:59:05 +0300

    Formatting the project

---
+++ src/main/java/com/soft/ressystem/RessystemApplication.java
@@ -0,0 +1,13 @@
+package com.soft.ressystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RessystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RessystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soft/ressystem/RessystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soft.ressystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RessystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
